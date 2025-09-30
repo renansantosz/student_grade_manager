2693aced24b0f9bed2351d5f72e1d3f8dda721f8
renansantosz <renanaraujocgsp@gmail.com>
2025-09-30 16:05:32 -0300

    GET AND POST

---
+++ index.js
@@ -0,0 +1,43 @@
+import http from 'http';
+import { v4 } from 'uuid';
+
+const port = 3000;
+const grades = [
+  {
+    id: v4(),
+    studentName: "Renan",
+    subject: "English",
+    grade: "8",
+  },
+];
+
+const server = http.createServer((request, response) => {
+  const { method, url } = request;
+  let body = "";
+
+  request.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  request.on("end", () => {
+    if (url === "/grades" && method === "GET") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(grades));
+    } 
+    else if (url === "/grades" && method === "POST") {
+      const { studentName, subject, grade } = JSON.parse(body);
+      const newGrade = { id: v4(), studentName, subject, grade };
+      grades.push(newGrade);
+      response.writeHead(201, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(newGrade));
+    } 
+    else {
+      response.writeHead(404, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ message: "Route not found" }));
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
